51f1e63d726efd71a570226c892be23e7f02ff15
ValentinaDej <107748584+ValentinaDej@users.noreply.github.com>
2023-11-18 14:37:09 +0200

    uath and addLink transformation

--- pro_shortlinker/src/functions/api/auth/signIn.ts
+++ pro_shortlinker/src/functions/api/auth/signIn.ts
@@ -1,19 +1,9 @@
 import UserService from "../../../services/userService";
 import handlerWrapper from "../../../helpers/handlerWrapper";
-import handlerError from "../../../helpers/handlerError";
 
 export const handler = handlerWrapper(async (event) => {
-  if (!event.body) {
-    handlerError.throwError(400, "Bad Request: No body provided");
-  }
-
   const body = JSON.parse(event.body || "{}");
-
   const { email, password } = body;
-
-  if (!email || !password) {
-    handlerError.throwError(400, "Bad Request: Missing email or password");
-  }
 
   const data = await UserService.login(email, password);
 
--- pro_shortlinker/src/functions/api/link/addLink.ts
+++ pro_shortlinker/src/functions/api/link/addLink.ts
@@ -1,22 +1,9 @@
 import LinkService from "../../../services/linkService";
-import handlerError from "../../../helpers/handlerError";
 import handlerWrapper from "../../../helpers/handlerWrapper";
 
 export const handler = handlerWrapper(async (event) => {
-  if (!event.body || event.body.trim() === "") {
-    handlerError.throwError(400, "Bad Request: No body provided");
-  }
-
   const body = JSON.parse(event.body || "{}");
-
   const { link, expiration_time } = body;
-  if (!link || !expiration_time) {
-    handlerError.throwError(
-      400,
-      "Bad Request: Missing link or expiration_time"
-    );
-  }
-
   const email = JSON.parse(event?.requestContext?.authorizer?.stringKey);
 
   const data = await LinkService.addLink(link, email, expiration_time);
